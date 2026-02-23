f404b088e60c996bafb2129a0b9c7239bd218ebd
anushka-codes11 <anshipandit631@gmail.com>
2026-02-23 21:50:58 +0530

    Update Dictionary Creation 23 feb .py

--- self-learning/Dictionary Creation 23 feb .py
+++ self-learning/Dictionary Creation 23 feb .py
@@ -4,4 +4,8 @@ student = {
     "age": 20,
     "course": "Python"
 }
-print("Student details:", student)
+
+print("Student details:", student) 
+
+// Output : 
+Student details: {'name': 'John', 'age': 20, 'course': 'Python'}
