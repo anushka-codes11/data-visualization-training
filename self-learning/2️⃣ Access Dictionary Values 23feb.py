648d68e4e0b7cd2903fdfbe88e0979679f4db6c0
anushka-codes11 <anshipandit631@gmail.com>
2026-02-23 21:55:54 +0530

    Update 2️⃣ Access Dictionary Values 23feb.py

--- self-learning/2️⃣ Access Dictionary Values 23feb.py
+++ self-learning/2️⃣ Access Dictionary Values 23feb.py
@@ -1,4 +1,9 @@
 # Accessing values using key
 student = {"name": "John", "age": 20}
 print("Name:", student["name"])
-print("Age:", student.get("age"))
+
+print("Age:", student.get("age"))  
+
+//Output : 
+ Name : John 
+ Age  : 20  
