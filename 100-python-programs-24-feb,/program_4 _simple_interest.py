02d791ec10610c77704ee484e9706d2bde943797
anushka-codes11 <anshipandit631@gmail.com>
2026-02-24 11:09:12 +0530

    Add files via upload

---
+++ 100-python-programs-24-feb,/program_4 _simple_interest.py
@@ -0,0 +1,17 @@
+# Read inputs from standard input
+P = float(input())   # Principal
+R = float(input())   # Rate
+T = float(input())   # Time
+
+# Calculate Simple Interest
+SI = (P * R * T) / 100
+
+# Print the result
+print(SI) 
+
+// input :
+   1000
+    5
+    2  
+// output: 
+   100.0
