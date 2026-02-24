2390e60e29c401f11b11d06b77dba3a1837552e2
anushka-codes11 <anshipandit631@gmail.com>
2026-02-24 11:25:21 +0530

    Add files via upload

---
+++ 100-python-programs-24-feb,/program_7_fahrenheit_to_celsius.py
@@ -0,0 +1,13 @@
+# Read temperature in Fahrenheit from standard input
+F = float(input())
+
+# Convert Fahrenheit to Celsius
+C = (F - 32) * 5/9
+
+# Print the result
+print(C)
+
+//input :
+  77
+//output:
+   25
