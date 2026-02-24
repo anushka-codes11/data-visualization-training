3d5e459887b9490ffc3b61b25fd03804649a7519
anushka-codes11 <anshipandit631@gmail.com>
2026-02-24 10:57:00 +0530

    Add files via upload

---
+++ 100-python-programs-24-feb,/👉 program2_check_number.py
@@ -0,0 +1,16 @@
+# Read input from standard input
+num = int(input())
+
+# Check the number
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero") 
+
+
+// input : 
+     7
+// output: 
+   positive 
