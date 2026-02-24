9fc261a20e009e35cc9618d0c2928a424cc23a37
anushka-codes11 <anshipandit631@gmail.com>
2026-02-24 12:16:09 +0530

    Add files via upload

---
+++ 100-python-programs-24-feb,/program_15_divisible_by_3_and_5.py
@@ -0,0 +1,13 @@
+# Read number from standard input
+num = int(input())
+
+# Check divisibility by both 3 and 5
+if num % 3 == 0 and num % 5 == 0:
+    print("Divisible by both 3 and 5")
+else:
+    print("Not divisible by both 3 and 5")
+
+// input : 
+    15 
+//output :  
+divisible by both 3 and 5
