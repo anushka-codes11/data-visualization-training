46794bb8f458706cf8670f3c34e0d248eb4bfff3
anushka-codes11 <anshipandit631@gmail.com>
2026-02-24 16:30:15 +0530

    Add files via upload

---
+++ 100-python-programs-24-feb,/p93_replace_vowels.py
@@ -0,0 +1,14 @@
+s = input()
+vowels = "aeiouAEIOU"
+result = ""
+for ch in s:
+    if ch in vowels:
+        result += "*"
+    else:
+        result += ch
+print(result)
+
+//input
+hello
+output
+h*ll*
