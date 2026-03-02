b5b805649628d4e4f6a196a4a230e525975a57d7
anushka-codes11 <anshipandit631@gmail.com>
2026-03-02 22:46:27 +0530

    Add files via upload

---
+++ self-learning/calculator.py
@@ -0,0 +1,20 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+
+if num2 != 0:
+    print("Division:", num1 / num2)
+else:
+    print("Division by zero is not allowed")
+
+// output:
+Enter first number: 4
+Enter second number: 6
+Addition: 10.0
+Subtraction: -2.0
+Multiplication: 24.0
+Division: 0.6666666666666666
+
